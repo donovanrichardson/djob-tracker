c09af952384da054648c7827c828de90258acccc
donovanrichardson <donovanspqa@gmail.com>
2020-09-12 19:31:33 -0400

    user model

--- authentication/models.py
+++ authentication/models.py
@@ -10,3 +10,62 @@ jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password = None):
+        if username is None:
+            raise TypeError("Users must have a username")
+        if email is None:
+            raise TypeError("Users must have an email")
+
+        user = self.model(
+            username = username,
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError("Superusers must have a password")
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    username = models.CharField(db_index=True, max_length=255, unique = True)
+    email = models.EmailField(db_index=True, unique = True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+
+    def _generate_jwt_token(self):
+        """
+        Generates a JSON Web Token that stores this user's instance and has an expiry
+        date set to 60 days into the future.
+        """
+        payload = jwt_payload_handler(self)
+        token = jwt_encode_handler(payload)
+
+        return token
+
+
